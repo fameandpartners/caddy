09f4aef11ae3dfd7d13f347226c1a6acc7c9d3e6
Douglas Sellers <douglas.sellers@gmail.com>
2017-11-14 00:19:43 -0800

    Started to fetch saved data

--- src/js/ProductsList.js
+++ src/js/ProductsList.js
@@ -2,9 +2,7 @@
 
 import React from 'react';
 import autoBind from 'react-autobind';
-import * as AppActions from './actions/AppActions';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import request from 'superagent';
 
 export default class ProductsList extends React.Component
 {
@@ -14,8 +12,17 @@ export default class ProductsList extends React.Component
         autoBind( this );
         this.state = {
             product: {
-             }
+            }
         };
+    }
+    componentDidMount()
+    {
+        let url = 'https://product-management-dev.firebaseio.com/products.json';
+        request.get( url ).end((error, response) => {
+            console.log( 'done with  get' );
+            console.log( response.text );
+        } );
+
     }
     render()
     {
