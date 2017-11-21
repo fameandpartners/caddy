9282c541ceea4683f27736dac82324c6a4054839
Douglas Sellers <douglas.sellers@gmail.com>
2017-11-20 16:59:44 -0800

    You can now add colors

---
+++ src/js/ExpandableList.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import autoBind from 'react-autobind';
+
+export default class ExpandableList extends React.Component
+{
+  constructor( props )
+  {
+    super( props );
+    autoBind( this );
+    this.state = {
+      contents: []
+    };
+
+    this.textBoxes = {};
+  }
+
+  updateItem(number)
+  {
+    let contents = this.state.contents;
+    contents[number] = this.textBoxes[number].value;
+    this.setState(
+      {
+        contents: contents
+      }
+    );
+    this.props.contentUpdate( contents );
+  }
+  renderItem( item, number )
+  {
+    return (
+      <li key={this.props.addType + "-item-" + number}>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-1">
+              {this.props.addType}
+            </div>
+            <div className="col-md-2">
+              <input type="text"
+                     defaultValue={item}
+                     onKeyUp={() => this.updateItem( number ) }
+                ref={(input) => { this.textBoxes[number] = input; }} />
+            </div>
+          </div>
+        </div>
+      </li>      
+    );
+    
+  }
+  componentDidMount()
+  {
+    this.updateWithLatestState( this.props );
+  }
+  
+  updateWithLatestState(props)
+  {
+
+    if( props.startingValue )
+    {
+      this.setState(
+        {
+          contents: props.startingValue
+        }
+      );
+    };
+  }
+  
+  componentWillReceiveProps( nextProps )
+  {
+
+    this.updateWithLatestState( nextProps );
+  }
+
+  add()
+  {
+    let contents = this.state.contents;
+    contents = contents.concat( [null] );
+    this.setState(
+      {
+        contents: contents
+      }
+    );
+  }
+  
+  render()
+  {
+    return (
+      <div className="row">
+        <div className="col-md-12">
+          <ol>
+            {this.state.contents.map( this.renderItem ) }
+          </ol>
+        </div>
+        <div className="row">
+          <div className="col-md-6">
+            <button onClick={this.add}>Add {this.props.addType}</button>
+          </div>
+        </div>
+      </div>
+    );
+    
+  }
+  
+}
--- src/js/ProductDetails.js
+++ src/js/ProductDetails.js
@@ -7,248 +7,281 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import {getBase64} from './Utils';
 import CanvasImage from './CanvasImage';
+import ExpandableList from './ExpandableList';
 
 class ProductDetails extends React.Component
 {
-    constructor( props )
-    {
-        super( props );
-        autoBind( this );
-        this.state = {
-            product: {
-                details: {}
-            }
-        };
+  constructor( props )
+  {
+    super( props );
+    autoBind( this );
+    this.state = {
+      product: {
+        details: {}
+      }
+    };
 
+  }
+
+  save()
+  {
+    let product = this.state.product;
+    product.details.id = this.productId.value;
+    product.details.name = this.productName.value;
+    product.details.priceAUD = this.productAUDPrice.value;
+    product.details.priceUSD = this.productUSDPrice.value;
+    
+    this.setState(
+      {
+        product
+      }
+    );
+    this.props.save( product );
+  }
+
+  clean( value )
+  {
+    if( value )
+    {
+      return value;
     }
-
-    save()
+    else
     {
-        let product = this.state.product;
-        product.details.id = this.productId.value;
-        product.details.name = this.productName.value;
-        product.details.priceAUD = this.productAUDPrice.value;
-        product.details.priceUSD = this.productUSDPrice.value;
-        
-        this.setState(
-            {
-                product
-            }
-        );
-        this.props.save( product );
-    }
-
-    clean( value )
-    {
-        if( value )
-        {
-            return value;
-        }
-        else
-        {
-            return '';
-        }
-                
-    }
-    updateWithLatestState( props )
-    {
-        this.setState( {
-            product: props.product
-        } );
-        
-        this.productId.value = this.clean(props.product.details.id);
-        this.productName.value = this.clean(props.product.details.name);
-        this.productAUDPrice.value = this.clean( props.product.details.priceAUD );
-        this.productUSDPrice.value = this.clean( props.product.details.priceUSD);
-    }
-
-    componentDidMount()
-    {
-
-        this.updateWithLatestState( this.props );
+      return '';
     }
     
-    componentWillReceiveProps( nextProps )
-    {
+  }
+  updateWithLatestState( props )
+  {
+    this.setState( {
+      product: props.product
+    } );
+    
+    this.productId.value = this.clean(props.product.details.id);
+    this.productName.value = this.clean(props.product.details.name);
+    this.productAUDPrice.value = this.clean( props.product.details.priceAUD );
+    this.productUSDPrice.value = this.clean( props.product.details.priceUSD);
+  }
 
-        this.updateWithLatestState( nextProps );
+  componentDidMount()
+  {
+
+    this.updateWithLatestState( this.props );
+  }
+  
+  componentWillReceiveProps( nextProps )
+  {
+
+    this.updateWithLatestState( nextProps );
+  }
+  
+  uploadProductRefImage( e )
+  {
+    let context = this;
+    const file = e.target.files[0];
+    getBase64(file).then(base64 => {
+      let product = context.state.product;
+      product.details.productRefImage = base64;
+      context.setState( {
+        product: product
+      } );
+    });
+  }
+  renderProductRefImage()
+  {
+    if( this.state.product.details.productRefImage )
+    {
+      return( <CanvasImage imageData={this.state.product.details.productRefImage} width={236} height={200}/> );
+
+    } else
+    {
+      return( <span>Upload: <input type="file" id="productRefImageUpload" name='prodRefImageUpload' onChange={this.uploadProductRefImage} /></span> );
     }
     
-    uploadProductRefImage( e )
+    
+  }
+  addOrRemove( lengthName )
+  {
+    let product = this.state.product;
+    if( product.details.lengths.indexOf( lengthName ) > -1 )
     {
-        let context = this;
-        const file = e.target.files[0];
-        getBase64(file).then(base64 => {
-            let product = context.state.product;
-            product.details.productRefImage = base64;
-            context.setState( {
-                product: product
-            } );
-        });
-    }
-    renderProductRefImage()
+      product.details.lengths.splice( product.details.lengths.indexOf( lengthName ), 1 );
+    } else
     {
-        if( this.state.product.details.productRefImage )
-        {
-            return( <CanvasImage imageData={this.state.product.details.productRefImage} width={236} height={200}/> );
-
-        } else
-        {
-            return( <span>Upload: <input type="file" id="productRefImageUpload" name='prodRefImageUpload' onChange={this.uploadProductRefImage} /></span> );
-        }
-                        
-        
-    }
-    addOrRemove( lengthName )
-    {
-        let product = this.state.product;
-        if( product.details.lengths.indexOf( lengthName ) > -1 )
-        {
-            product.details.lengths.splice( product.details.lengths.indexOf( lengthName ), 1 );
-        } else
-        {
-            product.details.lengths.push( lengthName );
-        }
-
-        this.setState(
-            {
-                product: product
-            }
-        );
+      product.details.lengths.push( lengthName );
     }
 
-    lengthExists( lengthName )
-    {
-        return this.state.product.details.lengths && this.state.product.details.lengths.indexOf( lengthName ) > -1;
-    }
-    render()
-    {
-        return (
+    this.setState(
+      {
+        product: product
+      }
+    );
+  }
+
+  lengthExists( lengthName )
+  {
+    return this.state.product.details.lengths && this.state.product.details.lengths.indexOf( lengthName ) > -1;
+  }
+
+  updateColors( colors )
+  {
+    let product = this.state.product;
+    product.details.colors = colors;
+
+    this.setState(
+      {
+        product: product
+      }
+    );
+  }
+  
+  render()
+  {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
             <div className="container">
               <div className="row">
-                <div className="col-md-6">
-                  <div className="container">
-                    <div className="row">
-                      <div className="col-md-2">
-                        Style Number:
-                      </div>
-                      <div className="col-md-2">
-                        <input type="text" ref={(input) => { this.productId = input;  }}/>
-                      </div>
-                    </div>              
-                    <div className="row">
-                      <div className="col-md-2">
-                        Product Name:
-                      </div>
-                      <div className="col-md-2">
-                        <input type="text" ref={(input) => { this.productName = input;  }}/>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-md-2">
-                        Price AUD:
-                      </div>
-                      <div className="col-md-1 text-right">
-                        $
-                      </div>
-                      <div className="col-md-1">
-                        <input type="text" ref={(input) => { this.productAUDPrice = input;  }}/>
-                      </div>
-                      
-                    </div>
-                    <div className="row">
-                      <div className="col-md-2">
-                        Price USD:
-                      </div>
-                      <div className="col-md-1 text-right">
-                        $
-                      </div>
-                      <div className="col-md-1">
-                        <input type="text" ref={(input) => { this.productUSDPrice = input;  }}/>
-                      </div>
-                      
-                    </div>
-                    <div className="row">
-                      <div className="col-md-12">
-                        Available Lengths
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-md-1 text-right">
-                        Mini: </div>
-                      <div className="col-md-1">
-                        <input defaultChecked={this.lengthExists( 'Mini' )} onClick={ () => this.addOrRemove( 'Mini' ) }type="checkbox"/>                        
-                      </div>
-                      <div className="col-md-1 text-right">
-                        Knee: 
-                      </div>
-                      <div className="col-md-1">
-                        <input defaultChecked={this.lengthExists( 'Knee' )} onClick={ () => this.addOrRemove( 'Knee' ) }type="checkbox"/>                        
-                      </div>
-                      <div className="col-md-1 text-right">
-                        Midi: 
-                      </div>
-                      <div className="col-md-1">
-                        <input defaultChecked={this.lengthExists( 'Midi' )} onClick={ () => this.addOrRemove( 'Midi' ) }type="checkbox"/>                        
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-md-1 text-right">
-                        Ankle: 
-                      </div>
-                      <div className="col-md-1">
-                        <input defaultChecked={this.lengthExists( 'Ankle' )} onClick={ () => this.addOrRemove( 'Ankle' ) }type="checkbox"/>
-                      </div>                      
-                      <div className="col-md-1 text-right">
-                        Maxi: 
-                      </div>
-                      <div className="col-md-1">
-                        <input defaultChecked={this.lengthExists( 'Maxi' )} onClick={ () => this.addOrRemove( 'Maxi' ) }type="checkbox"/>
-                      </div>
-                    </div>
-                    <div>
-                      <button onClick={this.save}>Save</button>
-                    </div>
-                  </div>
-                  
+                <div className="col-md-2">
+                  Style Number:
                 </div>
-                <div className="col-md-6">
-                  <div className="container">
-                    <div className="row">
-                      <div className="col-md-3">
-                        <center>Product Ref Image</center>
-                      </div>
-                      
-                      <div className="col-md-12">
-                        {this.renderProductRefImage()}
-                        
-                      </div>
-                    </div>
-                  </div>
+                <div className="col-md-2">
+                  <input type="text" ref={(input) => { this.productId = input;  }}/>
+                </div>
+              </div>              
+              <div className="row">
+                <div className="col-md-2">
+                  Product Name:
+                </div>
+                <div className="col-md-2">
+                  <input type="text" ref={(input) => { this.productName = input;  }}/>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-md-2">
+                  Price AUD:
+                </div>
+                <div className="col-md-1 text-right">
+                  $
+                </div>
+                <div className="col-md-1">
+                  <input type="text" ref={(input) => { this.productAUDPrice = input;  }}/>
+                </div>
+                
+              </div>
+              <div className="row">
+                <div className="col-md-2">
+                  Price USD:
+                </div>
+                <div className="col-md-1 text-right">
+                  $
+                </div>
+                <div className="col-md-1">
+                  <input type="text" ref={(input) => { this.productUSDPrice = input;  }}/>
+                </div>
+                
+              </div>
+              <div className="row">
+                <div className="col-md-12">
+                  Available Lengths
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-md-1 text-right">
+                  Mini: </div>
+                <div className="col-md-1">
+                  <input defaultChecked={this.lengthExists( 'Mini' )} onClick={ () => this.addOrRemove( 'Mini' ) }type="checkbox"/>                        
+                </div>
+                <div className="col-md-1 text-right">
+                  Knee: 
+                </div>
+                <div className="col-md-1">
+                  <input defaultChecked={this.lengthExists( 'Knee' )} onClick={ () => this.addOrRemove( 'Knee' ) }type="checkbox"/>                        
+                </div>
+                <div className="col-md-1 text-right">
+                  Midi: 
+                </div>
+                <div className="col-md-1">
+                  <input defaultChecked={this.lengthExists( 'Midi' )} onClick={ () => this.addOrRemove( 'Midi' ) }type="checkbox"/>                        
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-md-1 text-right">
+                  Ankle: 
+                </div>
+                <div className="col-md-1">
+                  <input defaultChecked={this.lengthExists( 'Ankle' )} onClick={ () => this.addOrRemove( 'Ankle' ) }type="checkbox"/>
+                </div>                      
+                <div className="col-md-1 text-right">
+                  Maxi: 
+                </div>
+                <div className="col-md-1">
+                  <input defaultChecked={this.lengthExists( 'Maxi' )} onClick={ () => this.addOrRemove( 'Maxi' ) }type="checkbox"/>
                 </div>
               </div>
             </div>
-        );
-    }                      
+
+            <div className="row top-margin">
+              <div className="col-md-12">
+                Colors
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <ExpandableList addType="Color" contentUpdate={this.updateColors} startingValue={this.state.product.details.colors}/>
+              </div>
+            </div>
+            <div className="row top-margin">
+              <div className="col-md-12">
+                Taxons
+              </div>
+            </div>
+
+            <div className="row top-margin">
+              <div>
+                <button onClick={this.save}>Save</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-6">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-3">
+                  <center>Product Ref Image</center>
+                </div>
+                
+                <div className="col-md-12">
+                  {this.renderProductRefImage()}
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }                      
 }
 
 function stateToProps(state)
 {
-    if( state.product.details.lengths == null )
-    {
-        state.product.details.lengths = [];
-    }
-    return { product: state.product };
+  if( state.product.details.lengths == null )
+  {
+    state.product.details.lengths = [];
+  }
+  return { product: state.product };
 }
 
 function dispatchToProps(dispatch)
 {
-    return {
-        save: ( value ) =>
-            {
-                dispatch(AppActions.updateProductDetails( value ));
-            }
-    };
+  return {
+    save: ( value ) =>
+      {
+        dispatch(AppActions.updateProductDetails( value ));
+      }
+  };
 }
 
 
