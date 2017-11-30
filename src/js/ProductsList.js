ae71f33861440e150f8a0ff6f74bc3a21b564ddc
Douglas Sellers <douglas.sellers@gmail.com>
2017-11-30 13:08:34 -0800

    Made things look a little nicer

--- src/js/ProductsList.js
+++ src/js/ProductsList.js
@@ -10,95 +10,98 @@ const FIREBASE_URL = process.env.FIREBASE_URL;
 
 class ProductsList extends React.Component
 {
-    constructor( props )
-    {
-        super( props );
-        autoBind( this );
-        this.state = {
-            products: {
-            }
-        };
-
-    }
-    
-    componentWillMount()
-    {
-        let url = FIREBASE_URL + '/products.json';
-        request.get( url ).end((error, response) => {
-            this.setState(
-                {
-                    products:  JSON.parse( response.text )
-                }
-            );
-        } );
-    }
-    
-    loadProduct( styleNumber, versionNumber )
-    {
-        this.props.load( styleNumber, versionNumber );
-    }
-    
-    renderProducts( productsJSON )
-    {
-        let toReturn = [];
-      let context = this;
-      if( productsJSON )
-      {
-        Object.keys(productsJSON).map(function(styleNumber, keyIndex) {
-            toReturn.push(
-                <li key={styleNumber}>
-                  <div className="row">
-                    <div className="col-md-2">
-                      <span>{styleNumber}</span>
-                    </div>
-                    <div className="col-md-2">
-                      <span>
-                        <button onClick={() => context.loadProduct( styleNumber, productsJSON[styleNumber].version )}>Load Product</button>
-                      </span>
-                    </div>
-                  </div>
-                </li> );
-        });
-        return toReturn;
-      } else
-      {
-        return "";
+  constructor( props )
+  {
+    super( props );
+    autoBind( this );
+    this.state = {
+      products: {
       }
-    }
-    
-    render()
+    };
+
+  }
+  
+  componentWillMount()
+  {
+    let url = FIREBASE_URL + '/products.json';
+    request.get( url ).end((error, response) => {
+      this.setState(
+        {
+          products:  JSON.parse( response.text )
+        }
+      );
+    } );
+  }
+  
+  loadProduct( styleNumber, versionNumber )
+  {
+    this.props.load( styleNumber, versionNumber );
+  }
+  
+  renderProducts( productsJSON )
+  {
+    let toReturn = [];
+    let context = this;
+    if( productsJSON )
     {
-        return (
-            <div>
-              Products To Load
-              <ol>
-                {this.renderProducts( this.state.products )}
-              </ol>
-              <button onClick={() => this.props.newProduct()}>New Product</button>
+      Object.keys(productsJSON).map(function(styleNumber, keyIndex) {
+        toReturn.push(
+          <li key={styleNumber}>
+            <div className="container product-list-entry">
+              <div className="row">
+                <div className="col-md-2">
+                  <span>{styleNumber}</span>
+                </div>
+                <div className="col-md-2">
+                  <span>
+                    <button onClick={() => context.loadProduct( styleNumber, productsJSON[styleNumber].version )}>Load Product</button>
+                  </span>
+                </div>
+              </div>
+
             </div>
-        );
+          </li> );
+      });
+      return toReturn;
+    } else
+    {
+      return "";
     }
+  }
+  
+  render()
+  {
+    return (
+      <div>
+        Products To Load
+        <ol>
+          {this.renderProducts( this.state.products )}
+        </ol>
+        <button onClick={() => this.props.newProduct()}>New Product</button>
+      </div>
+    );
+  }
 }
 
 
 
 function stateToProps(state)
 {
-    return { };
+  return { };
 }
 
 function dispatchToProps(dispatch)
 {
-    return {
-        load: ( styleNumber, versionNumber ) =>
-            {
-              dispatch( AppActions.loadProduct( styleNumber, versionNumber ) );
-            },
-        newProduct: () =>
-            {
-                dispatch(AppActions.newProduct());
-            }
-    };
+  return {
+    load: ( styleNumber, versionNumber ) =>
+      {
+        dispatch( AppActions.loadProduct( styleNumber, versionNumber ) );
+      },
+    newProduct: () =>
+      {
+        dispatch(AppActions.newProduct());
+      }
+  };
 }
 
 export default connect(stateToProps, dispatchToProps)(ProductsList);
