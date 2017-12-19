13e5196a5669c831a14de2293aa05d9ab2d3dd79
Douglas Sellers <douglas.sellers@gmail.com>
2017-12-18 17:51:16 -0800

    Added in all of the details

--- src/js/UploadProduct.js
+++ src/js/UploadProduct.js
@@ -30,7 +30,14 @@ class UploadProduct extends React.Component
   {
     return {
       colors: this.state.product.details.colors ? this.state.product.details.colors : [],
-      lengths: this.state.product.details.lengths ? this.state.product.details.lengths.map( (element) => this.buildLength( element )) : []
+      lengths: this.state.product.details.lengths ? this.state.product.details.lengths.map( (element) => this.buildLength( element )) : [],
+      name: this.state.product.details.name,
+      price_aud: this.state.product.details.priceAUD,
+      price_usd: this.state.product.details.priceUSD,
+      primary_image: this.state.product.details.primaryImage ? "https://d1msb7dh8kb0o9.cloudfront.net/spree/products/38022/original/fp2615-navy-1.jpg?1509656449" : "",
+      secondary_images: this.state.product.details.secondaryImages ? this.state.product.details.secondaryImages.map( () => "https://d1msb7dh8kb0o9.cloudfront.net/spree/products/38021/original/fp2615-navy-2.jpg?1509656448" ) : "",
+      taxons: this.state.product.details.taxons ? this.state.product.details.taxons : []
+      
     };
   }
 
